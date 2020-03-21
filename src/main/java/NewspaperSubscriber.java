ce25af3e5111eec25f4432d85c84ec17213fb923
crimsonfyza <kevinmeeuwessen@outlook.com>
2020-03-21 13:19:53 +0100

    Initial commit

---
+++ src/main/java/NewspaperSubscriber.java
@@ -0,0 +1,23 @@
+public class NewspaperSubscriber implements Observer {
+
+    private String name;
+    private Subject topic;
+
+    public NewspaperSubscriber(String nm){
+        this.name=nm;
+    }
+    @Override
+    public void update() {
+        String msg = (String) topic.getUpdate(this);
+        if(msg == null){
+            System.out.println(name+": No new message");
+        }else
+            System.out.println(name+": "+msg);
+    }
+
+    @Override
+    public void setSubject(Subject sub) {
+        this.topic=sub;
+    }
+
+}
---
+++ src/main/java/ObserverPatternTest.java
@@ -0,0 +1,33 @@
+public class ObserverPatternTest {
+
+    public static void main(String[] args) {
+        //create subject
+        Newspaper topic = new Newspaper();
+
+        //create observers
+        Observer subscriber1 = new NewspaperSubscriber("Kevin");
+        Observer subscriber2 = new NewspaperSubscriber("Mark");
+        Observer subscriber3 = new NewspaperSubscriber("Aron");
+
+        //register observers to the subject
+        topic.register(subscriber1);
+        topic.register(subscriber2);
+        topic.register(subscriber3);
+
+        //attach observer to subject
+        subscriber1.setSubject(topic);
+        subscriber2.setSubject(topic);
+        subscriber3.setSubject(topic);
+
+        //test subscriber1 los
+        subscriber1.update();
+
+        //Controleer alle subscribers iteratief en print degene met een update.
+        topic.postMessage("Het dagblad is geupload!");
+
+        // test subscriber1 los
+        subscriber1.update();
+
+    }
+
+}
